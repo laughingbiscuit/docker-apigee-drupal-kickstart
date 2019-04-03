c7eff78e218b9fa12925895ee1dc766d080909ee
Sean Davis <43555662+laughingbiscuit@users.noreply.github.com>
2019-04-03 15:23:47 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,4 @@ docker build -t lb/d8 .
 docker run --name some-d8 -p 8080:80 -d \
 	-e APIGEE_EDGE_AUTH_TYPE=basic \
 	-e APIGEE_EDGE_ENDPOINT=https://api.enterprise.apigee.com/v1 \
-	-e APIGEE_EDGE_ORGANIZATION=$APIGEE_ORG \
-	-e APIGEE_EDGE_USERNAME=$APIGEE_USER \
-	-e APIGEE_EDGE_PASSWORD=$APIGEE_PASS \
 	lb/d8 
